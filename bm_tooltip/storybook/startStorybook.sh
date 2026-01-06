9263eb481bae8f0048609f2db6fa504f2ac4a5a1
joelbox <joelbox@users.noreply.github.com>
2026-01-06 12:24:41 +0100

    Automatic sync operation

--- bm_tooltip/storybook/startStorybook.sh
+++
@@ -1,6 +0,0 @@
-cd web/modules/custom/bm_tooltip/storybook
-npm install
-npm install @storybook/html-vite --save-dev
-npx storybook dev -p 6006 --host 0.0.0.0 --ci
-
-echo "access it at http://127.0.0.1:6006"
